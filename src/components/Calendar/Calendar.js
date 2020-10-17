b94bbf9fcb31f5d80ca2d256135974249d6f2655
Valentin <valek917@yandex.ru>
2020-10-17 13:53:18 +0300

    commit7

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -17,17 +17,17 @@ const Calendar = () => {
 					<ul className="calendar-list">
 						{
 							data.map(item => (
-							<li className="calendar-item" key={item.id}>
-								<article className="launches">
-									<div className="launches-image">
-										<img src={item.links.match.small} alt="" />
-									</div>
-									<div className="launches-content">
-										<h2 className="launches-title">{item.name}</h2>
-										<Link to={`/details/${item.id}`} className="button launches-details">Подробнее</Link>
-									</div>
-								</article>
-							</li>
+								<li className="calendar-item" key={item.id}>
+									<article className="launches">
+										<div className="launches-image">
+											<img src={item.links.patch.small} alt="" />
+										</div>
+										<div className="launches-content">
+											<h2 className="launches-title">{item.name}</h2>
+											<Link to={`/details/${item.id}`} className="button launches-details">Подробнее</Link>
+										</div>
+									</article>
+								</li>
 							))
 						}
 					</ul>
